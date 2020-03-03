f2d78d09798a50bf2ce70aa7428b088da0f2364b
Justin Byun <klofseattle@hotmail.com>
2020-03-02 17:36:00 -0800

    Clean up ignored files

---
+++ contextApp/src/components/BookList.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+export default class BookList extends Component {
+  render() {
+    return (
+      <div className="book-list">
+        <ul>
+          <li>Empire Noir</li>
+          <li>Giselle</li>
+          <li>Million KIsses</li>
+          <li>Somewhere Elevated</li>
+        </ul>
+      </div>
+    );
+  }
+}
